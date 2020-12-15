96b4bf43fb357a1cbb86ece8d61cfb82b1afa8db
Vikas-K-Repo <vikasvks1998@gmail.com>
2020-12-15 10:12:55 +0530

    Second Commit

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+/*This is an example program*/
+
+import java.util.*;
+import java.io.*;
+
+public class Hello_World
+{
+	public static void public static void main(String[] args) {
+		System.out.print("Hello Vikas");
+	}
+}
